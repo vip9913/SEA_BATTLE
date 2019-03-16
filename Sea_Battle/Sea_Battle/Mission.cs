4d85fd2b41e00a688e48d1631054018617e2da2d
vip9913 <alex_velig@mail.ru>
2019-03-16 23:13:49 +0300

    Добавлен класс Миссия

---
+++ Sea_Battle/Sea_Battle/Mission.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sea_Battle
+{
+    class Mission
+    {
+            Море sea;
+            Random rand;
+
+            public Mission(Море sea)
+            {
+                this.sea = sea;
+                rand = new Random();
+            }
+
+            public Статус Fight(out Точка target)
+            {
+                do
+                {
+                    target = new Точка(
+                        rand.Next(0, Море.размер_моря.x),
+                        rand.Next(0, Море.размер_моря.y));
+                } while (sea.КартаПопаданий(target) != Статус.неизвестно);
+                return sea.Выстрел(target);
+            }
+        }
+}
+
+
